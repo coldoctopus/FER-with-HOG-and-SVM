14af60e8887e3201503312958a5751f460994fc9
Huỳnh Thiên Bảo <legendrequiem0310@gmail.com>
2025-08-02 10:30:32 +0700

    udapte: add spliting option by ratio

--- src/hog.py
+++ src/hog.py
@@ -1,16 +1,26 @@
 import cv2, os
 from skimage import color, feature, io
 
-def preprocessing_split(filePath):
+def preprocessing_split(filePath, train_ratio=0.75):
     data_train = []
     label_train = []
     data_test = []
     label_test = []
-    face = ['anger', 'happy', 'neutral', 'sad', 'suprise']  #
+    face = ['anger', 'happy', 'neutral', 'sad', 'suprise']  # list of expressions
+
     for expression in face:
         directory = os.path.join(filePath, expression)  # use os.path.join for joining paths
         data_listing = os.listdir(directory)  # a list of path to each image
-        order = 1
+        total_images = len(data_listing)
+        train_count = int(train_ratio * total_images)
+
+        # catch the error if not having enough images for train or test
+        if train_count == 0:
+            raise ValueError(f"Train ratio too low — no images left for training in class '{expression}'")
+        if total_images - train_count == 0:
+            raise ValueError(f"Train ratio too high — no images left for testing in class '{expression}'")
+        
+        order = 1  # to count the number of images processed
         for file in data_listing:
             # open image in the main dataset folder + subfolder, then config them
             image = io.imread(os.path.join(directory, file))
@@ -18,11 +28,11 @@ def preprocessing_split(filePath):
                 image = color.rgb2gray(image)
             image = cv2.resize(image, (200, 200))
 
-            # build hog feat.
+            # build hog features
             hog_features = feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), block_norm='L2-Hys', visualize=False)
 
             # add them to lists
-            if order<=375:
+            if order<=train_count:
                 data_train.append(hog_features)
                 label_train.append(expression)
                 order+=1
@@ -49,7 +59,6 @@ def preprocessing_full(filePath):
 
             # build hog feat.
             hog_features = feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), block_norm='L2-Hys', visualize=False)
-
 
             data.append(hog_features)
             label.append(expression)
